f8c4703ac97227cdb59e4dd64e049b05b480774a
Виктор Романов <rjapolov1@gmail.com>
2020-09-22 21:42:40 +0300

    ффвфыв

--- Car/AuthOptions.cs
+++ Car/AuthOptions.cs
@@ -8,7 +8,7 @@ namespace TokenApp
         public const string ISSUER = "MyAuthServer"; // издатель токена
         public const string AUDIENCE = "MyAuthClient"; // потребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
-        public const int LIFETIME = 60000; // время жизни токена s
+        public const int LIFETIME = 60000; // время жизни токена sыфвфыв
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
