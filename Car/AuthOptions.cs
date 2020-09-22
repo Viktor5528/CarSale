7fb4c24af41fb5d674247abdf47ed29a4de070ff
Виктор Романов <rjapolov1@gmail.com>
2020-09-22 21:43:27 +0300

    eqwe

--- Car/AuthOptions.cs
+++ Car/AuthOptions.cs
@@ -6,7 +6,7 @@ namespace TokenApp
     public class AuthOptions
     {
         public const string ISSUER = "MyAuthServer"; // издатель токена
-        public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+        public const string AUDIENCE = "MyAuthClient"; // потqweqweqребитель токена
         const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
         public const int LIFETIME = 60000; // время жизни токена sыфвфыв
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
